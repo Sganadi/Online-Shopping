134b57fc86b415d04ca7bc1aef2d9d688283e7f1
Sganadi <79947073+Sganadi@users.noreply.github.com>
2021-04-30 15:17:40 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','saik.cavwurvdgaeu.us-east-2.rds.amazonaws.com');
+define('DB_USER','admin');
+define('DB_PASS' ,'admin123');
 define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
